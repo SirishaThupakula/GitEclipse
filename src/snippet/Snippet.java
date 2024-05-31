8cd01539b3919b1d5e3480e940ed902d2880d403
SirishaThupakula <32529610+SirishaThupakula@users.noreply.github.com>
2024-05-31 16:20:10 +0900

    Update Snippet.java

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -2,7 +2,9 @@ package snippet;
 
 public class Snippet {
 	public static void main(String[] args) {
+		//this is for testing
 		System.out.println("hello");
+		System.out.println("konichiwa");
 	}
 }
 
